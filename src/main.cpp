994aa83747a474c25e908ae4292e47caff6ef28d
marcus poulton <marcus.poulton@gmail.com>
2020-08-19 08:56:09 +0100

    initial

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,13 @@
 void setup()
 {
 	//The 8-pin ATTinys (25/45/85) all have 5 usable digital output pins 0-4
-	pinMode(0, OUTPUT);
+	pinMode(4, OUTPUT);
 }
 void loop()
 {
-	digitalWrite(0, HIGH);  	//Set the LED pins to HIGH. This gives power to the LED and turns it on
+	digitalWrite(4, HIGH);  	//Set the LED pins to HIGH. This gives power to the LED and turns it on
 	delay(500);  	// Wait for a half a second
 
-	digitalWrite(0, LOW); 	// Set the LED pins to LOW. This turns it off
+	digitalWrite(4, LOW); 	// Set the LED pins to LOW. This turns it off
 	delay(1000);    // Wait for a half second
 }
