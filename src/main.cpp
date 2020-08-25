bc7069e588d8eb21ecd73edd19b9c4b67590e5c0
marcus poulton <marcus.poulton@gmail.com>
2020-08-25 08:48:32 +0100

    initial

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,15 @@
 #include <arduino.h>
+
+
+
+#define LED PB4
 void setup()
 {
 	//The 8-pin ATTinys (25/45/85) all have 5 usable digital output pins 0-4
-	pinMode(4, OUTPUT);
+	pinMode(LED, OUTPUT);
 }
 void loop()
 {
-	digitalWrite(4, HIGH);  	//Set the LED pins to HIGH. This gives power to the LED and turns it on
-	delay(500);  	// Wait for a half a second
-
-	digitalWrite(4, LOW); 	// Set the LED pins to LOW. This turns it off
-	delay(1000);    // Wait for a half second
+	digitalWrite(LED, HIGH);  	//Set the LED pins to HIGH. This gives power to the LED and turns it on
+	digitalWrite(LED, LOW); 	// Set the LED pins to LOW. This turns it off
 }
